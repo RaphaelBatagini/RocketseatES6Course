c3c7c9f8b2cccdd4a85b1ee71e951778fc747bec
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-06-24 08:20:32 -0300

    Finish challenge four

--- scripts/module1/challenge4.js
+++ scripts/module1/challenge4.js
@@ -18,3 +18,22 @@ const { nome, endereco: { cidade, estado } } = empresa;
 console.log(nome);
 console.log(cidade);
 console.log(estado);
+
+/*
+ * Na seguinte função:
+ *
+ * function mostraInfo(usuario) {
+ *  return `${usuario.nome} tem ${usuario.idade} anos.`;
+ * }
+ * mostraInfo({ nome: 'Diego', idade: 23 })
+ * 
+ * Utilize a desestruturação nos parâmetros da função para buscar o nome e idade do usuário
+ * separadamente e a função poder retornar apenas:
+ * 
+ * return `${nome} tem ${idade} anos.`;
+ */
+
+function mostraInfo({ nome, idade }) {
+    return `${nome} tem ${idade} anos.`;
+}
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
