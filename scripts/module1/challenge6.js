fa326cefeacd0c3e42f0dc1443948fa62aab6259
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-06-24 09:25:24 -0300

    Add solutions for challenge six

---
+++ scripts/module1/challenge6.js
@@ -0,0 +1,12 @@
+// Template literals
+
+/*
+ * Converta o seguinte trecho de código utilizando Template Literals:
+ * const usuario = 'Diego';
+ * const idade = 23;
+ * console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+ */
+
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario} possui ${idade} anos`);
